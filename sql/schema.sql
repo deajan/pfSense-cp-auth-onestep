f180f5d81609d8cb46b518df3a74d38e04f16580
Python-sublab <elp342@gmail.com>
2017-07-07 18:00:21 -0600

    Update schema.sql
    This is a change needed in order for the script to work correctly in pf sense 2.3.4, more specifically with freeRadius 3

--- sql/schema.sql
+++ sql/schema.sql
@@ -60,7 +60,7 @@ CREATE TABLE radcheck (
   id int(11) unsigned NOT NULL auto_increment,
   username varchar(64) NOT NULL default '',
   attribute varchar(64)  NOT NULL default '',
-  op char(2) NOT NULL DEFAULT '==',
+  op char(2) NOT NULL DEFAULT ':=',
   value varchar(253) NOT NULL default '',
   PRIMARY KEY  (id),
   KEY username (username(32))
